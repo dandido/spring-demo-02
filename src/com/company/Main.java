dbfdcbc18d6527e603cc9768fb25c51f6bd6c405
Hajjar Med Chedly <chhajjar2006@gmail.com>
2021-08-04 00:14:42 +0100

    Demo 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        //read spring config file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        //get bean from spring container
+       // Coach coach = context.getBean("sellytennisCoach",Coach.class); Named
+        Coach coach = context.getBean("tennisCoach",Coach.class);// use name of the class with lower case 1st letter
+        //call a method on bean
+        System.out.println(coach.DailyWorkout());
+        //close the context
+        context.close();
     }
 }
--- src/com/company/TennisCoach.java
+++ src/com/company/TennisCoach.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import org.springframework.stereotype.Component;
+
+//@Component("sellytennisCoach") Named
+@Component
 public class TennisCoach implements Coach{
     @Override
     public String DailyWorkout() {
-        return null;
+        return "TennisCoach : DailyWorkout";
     }
 }
