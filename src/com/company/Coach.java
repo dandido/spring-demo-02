9759026acffeee9cd8078cd7f77d189c37af0a6c
Hajjar Med Chedly <chhajjar2006@gmail.com>
2021-08-04 00:01:57 +0100

    Just to test the synch

--- src/com/company/Coach.java
+++ src/com/company/Coach.java
@@ -1,5 +1,5 @@
 package com.company;
 
 public interface Coach {
-
+    public String DailyWorkout();
 }
---
+++ src/com/company/TennisCoach.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class TennisCoach implements Coach{
+    @Override
+    public String DailyWorkout() {
+        return null;
+    }
+}
